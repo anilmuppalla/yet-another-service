b408c77a16b94518172938a2c47c0cfbe4f7f65e
Anil Muppalla <anil.kumar.848@gmail.com>
2018-09-09 20:54:22 -0400

    test illegal

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -7,17 +7,20 @@ import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class ApplicationTest {
     @Test
     fun getAuthorTest() = withTestApplication(Application::main) {
         val gson = Gson()
         val expected = Author("anil", "example.com")
+        val illegal = Author("kumar", "")
         handleRequest(HttpMethod.Get, "/author") {
 
         }.response.let {
             val result = gson.fromJson<Author>(it.content, Author::class.java)
             assertEquals(expected, result)
+            assertNotEquals(illegal, result)
         }
     }
 }
